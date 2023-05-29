970a345eaa051f4c1345cf2a5276b1a5227428a6
hemant-truemeds <hemant.jadhav@truemeds.in>
2023-05-29 15:14:08 +0530

    use App selector added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
+import { TypedUseSelectorHook, useDispatch } from "react-redux";
 
-import rootReducer from "./reducers";
+import rootReducer, { RootState } from "./reducers";
+import { useSelector } from "react-redux";
 
 const store = configureStore({ reducer: rootReducer });
 
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
